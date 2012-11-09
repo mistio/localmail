4073ff64110a93167efb857873d2c19dfc7ef30c
Simon Davy <bloodearnest@gmail.com>
2012-11-09 14:15:58 +0000

    improve test runner

--- test.sh
+++ test.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
+
+run_old()
+{
+    echo -n "Running $1..."
+    python $1 &> .log
+    if [ $? -eq 0 ]
+    then
+        echo "passed"
+    else
+        echo "failed"
+        echo "Log: "
+        cat .log
+    fi
+    rm -f .log
+}
+
+echo -n "Starting localmail..."
 twistd localmail
+echo "done"
+
+echo "Running new funtional tests"
 python -m unittest discover -s localmail -b
+
+run_old localmail/tests/simple.py
 sh kill_localmail.sh
+run_old localmail/tests/thread.py
